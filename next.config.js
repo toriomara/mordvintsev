eea16229eeb37057d3c8390252876f71034247c0
toriomara <budkevich.vladimir@gmail.com>
2023-11-04 19:20:24 +0300

    fix: add plugins in config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
+import NormalModuleReplacementPlugin from "webpack";
+
 const nextConfig = {
   images: {
     domains: ['tailwindui.com', 'unsplash.com', 'images.unsplash.com', 'firebasestorage.googleapis.com']
   },
+  plugins: [
+    new NormalModuleReplacementPlugin(
+      /email\/render/,
+      path.resolve(__dirname, "./src/renderEmailFix.js"),
+    ),
+  ],
 }
 
 module.exports = nextConfig
