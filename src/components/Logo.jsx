e6c32c200977d1485ccc6b8e3f12b664b0cf55a4
Vladimir Budkevich <budkevich.vladimir@gmail.com>
2024-03-11 21:45:44 +0300

    fix: logo toggle_2

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,9 +4,16 @@ import Image from "next/image";
 import Link from "next/link";
 import { useTheme } from "next-themes";
 import { useEffect } from "react";
+import dynamic from "next/dynamic";
+
+import LogoWhite from "../../public/logo-white.svg";
+import LogoBlack from "../../public/logo.svg";
 
 export const Logo = () => {
   const { theme } = useTheme();
+
+  // const WhiteLogo = dynamic(() => import("./../../public/logo-white.svg"), { ssr: false });
+  // const BlackLogo = dynamic(() => import("./../../public/logo.svg"), { ssr: false });
 
   const darkTheme =
     typeof window !== "undefined" &&
@@ -14,8 +21,12 @@ export const Logo = () => {
 
   const srcLogo =
     theme === "dark" || (theme === "system" && darkTheme)
-      ? "logo-white.svg"
-      : "logo.svg";
+    ? "logo-white.svg"
+    : "logo.svg";
+    // ? LogoWhite
+    // : LogoBlack
+  // ? WhiteLogo
+  // : BlackLogo;
 
   useEffect(() => {
     srcLogo;
@@ -25,11 +36,13 @@ export const Logo = () => {
   //   console.log(window.innerWidth);
   // }
 
-  console.log(darkTheme);
-  
+  console.log(srcLogo);
+
   return (
     <Link className="max-w-max" href="/">
-      <Image src={srcLogo} width={200} height={20} alt="Лого" />
+      <Image className="hidden dark:block" src={LogoWhite} width={195} height={19} alt="Лого" />
+      <Image className="block dark:hidden" src={LogoBlack} width={195} height={19} alt="Лого" />
+      {/* <Image src={srcLogo} width={200} height={20} alt="Лого" /> */}
     </Link>
   );
 };
--- src/components/LogoWithNoSSR.jsx
+++ src/components/LogoWithNoSSR.jsx
@@ -1,8 +1,11 @@
+// "use client";
 import dynamic from "next/dynamic";
 
 const LogoWithNoSSR = dynamic(() => import("./Logo"), {
   ssr: false,
 });
+
+// export default () => <LogoWithNoSSR />
 
 export default function LogoWithNoSSR2() {
   <LogoWithNoSSR />;
