39cbcd04f91fcb1537ed10dd3a7648f2e6cf26f4
Vladimir Budkevich <budkevich.vladimir@gmail.com>
2024-11-16 18:46:07 +0300

    hideResend

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,18 +1,18 @@
-import { NextResponse } from "next/server";
-import { Resend } from "resend";
+// import { NextResponse } from "next/server";
+// import { Resend } from "resend";
 
-export async function GET() {
-  const resend = new Resend(process.env.RESEND_API_KEY);
+// export async function GET() {
+//   const resend = new Resend(process.env.RESEND_API_KEY);
 
-  try {
-    const { data } = await resend.emails.send({
-      from: "next@next.ru",
-      to: "next@resend.com",
-      subject: "Hello from the other side",
-      html: "<h1>Hello from the NEXT.JS</h1>",
-    });
-    return NextResponse.json({ data });
-  } catch (error) {
-    return NextResponse.json({ error });
-  }
-}
+//   try {
+//     const { data } = await resend.emails.send({
+//       from: "next@next.ru",
+//       to: "next@resend.com",
+//       subject: "Hello from the other side",
+//       html: "<h1>Hello from the NEXT.JS</h1>",
+//     });
+//     return NextResponse.json({ data });
+//   } catch (error) {
+//     return NextResponse.json({ error });
+//   }
+// }
